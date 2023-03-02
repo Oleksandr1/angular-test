e049f59ce60d96dba4771f8f613ab2e202be44f7
Oleksandr Shcherbyna <sanya071186@gmail.com>
2023-03-02 12:53:47 +0200

    add decimal.js

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import {Decimal } from 'decimal.js'
+Decimal.set({precision: 18})
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
